8b5135ce13f35ea2262bd63a68592a5c78664e46
nickstagl <nick.stagl@kellogg.northwestern.edu>
2026-02-24 03:55:26 +0000

    places list and rotuning to log page built

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,25 @@
+class PlacesController < ApplicationController
+  def index
+    #find all Places rows
+    @places = Place.all
+    #render places/index
+  end
+
+  def show
+    @id = params["id"]
+    @place = Place.find_by({"id" => @id})
+  end
+
+  def create
+    place = Place.new
+    place["name"] = params["name"]
+
+    place.save
+    #redirect user
+    redirect_to "/places"
+  end
+  def new
+
+    #render view with new Place form
+  end
+end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   # talks to the "places" table
 end
